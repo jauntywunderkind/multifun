78dc488766f21ae9d0b5d5b02ef6928c7c6ec7e6
jauntywunderkind <jaunty+wunder+kind+git@eldergods.com>
2019-04-15 00:18:47 -0400

    Fix test imports.

--- .test/index.js
+++ .test/index.js
@@ -1,16 +1,17 @@
-import { multifun, Reduce, FirstReducer} from "../multifun"
+"use module"
+import { multifunc, Reduce, FirstReducer} from "../multifunc"
 import tape from "tape"
 
 tape( "can run multiple things", function( t){
 	let a= 1
-	new multifun(()=> a++, ()=> a++, ()=> a++)()
+	new multifunc(()=> a++, ()=> a++, ()=> a++)()
 	t.equal( a, 4, "three incrementing functions have been run")
 	t.end()
 })
 
 tape( "first reducer runs", function( t){
 	let a= 1
-	const m= new multifun(()=> a++, ()=> a++, ()=> a++)
+	const m= new multifunc(()=> a++, ()=> a++, ()=> a++)
 	m[ Reduce]= FirstReducer
 	m()
 	t.equal( a, 4, "three incrementing functions have been run")
@@ -19,7 +20,7 @@ tape( "first reducer runs", function( t){
 
 tape( "Can push a new function", function( t){
 	let a= 1
-	const m= new multifun(()=> a++, ()=> a++, ()=> a++)
+	const m= new multifunc(()=> a++, ()=> a++, ()=> a++)
 	m.push(()=> a++)
 	m[ Reduce]= FirstReducer
 	m()
